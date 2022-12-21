2164957fa65f7a0314c7dfb57c339625638e2178
benito011 <120726069+benito011@users.noreply.github.com>
2022-12-21 18:13:25 +0530

    Add files via upload

---
+++ FileUploadDownload/src/test/java/net/codejava/api/upload/FileUploadResponse.java
@@ -0,0 +1,25 @@
+package net.codejava.api.upload;
+
+public class FileUploadResponse {
+	private String fileName;
+    public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getDownloadUri() {
+		return downloadUri;
+	}
+	public void setDownloadUri(String downloadUri) {
+		this.downloadUri = downloadUri;
+	}
+	public long getSize() {
+		return size;
+	}
+	public void setSize(long size) {
+		this.size = size;
+	}
+	private String downloadUri;
+    private long size;
+}
